89e87ff1360ff565efea1268d7ebfd5ff0929c47
besnn <besniknuro74@gmail.com>
2024-12-18 16:56:29 +0100

    change settings to reflect postgres usage instead of sqlite delete sqlite database update requirements.txt with postgres driver

--- WebpageMonitorBackend/settings.py
+++ WebpageMonitorBackend/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = "WebpageMonitorBackend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "webpage_monitor",
+        "USER": "postgres",
+        "PASSWORD": "TMP_PASSWORD_",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
